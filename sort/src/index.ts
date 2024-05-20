4940fe03d80f6afaf51be8f940a08ca91eaf5cb4
Ishan-Pradhan <pradhan.ishan70@gmail.com>
2024-05-20 22:15:24 +0545

    added abstraction to classes

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,21 @@
 import { CharactersCollection } from "./CharacterCollection";
+import { LinkedList } from "./LinkedList";
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 1000]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([10, 3, -5, 1000]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("ohYeah");
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
