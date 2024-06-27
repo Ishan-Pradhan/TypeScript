586df53d8b78f356d52546a7109b4d16c4b7e856
Ishan-Pradhan <pradhan.ishan70@gmail.com>
2024-06-27 08:39:47 +0545

    Decorators

---
+++ decorators/decorators.ts
@@ -0,0 +1,28 @@
+class Boat {
+  color: string = "red";
+
+  get formattedColor(): string {
+    return `This boats color is ${this.color}`;
+  }
+
+  @logError("something bad")
+  pilot(): void {
+    throw new Error();
+    console.log("swish");
+  }
+}
+
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+    desc.value = function () {
+      try {
+        method();
+      } catch (error) {
+        console.log(errorMessage);
+      }
+    };
+  };
+}
+
+new Boat().pilot();
